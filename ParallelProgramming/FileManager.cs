5a1fe09c42dffc3bc180f5b2c513e6ce5efded32
RakeshRaviGovindhan <rakeshravig001@gmail.com>
2024-06-23 23:43:17 +0530

    TPL Code added

---
+++ ParallelProgramming/FileManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming
+{
+    public class FileManager
+    {
+        internal void ReadFile()
+        {
+            using (FileStream)
+            {
+
+            }
+        }
+    }
+}
